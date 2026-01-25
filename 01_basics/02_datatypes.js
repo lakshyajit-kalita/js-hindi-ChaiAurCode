45b8105636edd40502eb35eaebec17cdea82445f
Lakshyajit kalita <144526326+lakshyajit-kalita@users.noreply.github.com>
2026-01-25 15:17:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+let name  = "lakshya"
+let age  = 20
+let isLogedin  = true
+let state  = null // or let state;
+
+// number  => 2 to power 53
+// bigint 
+// string  => ""
+// boolean  => true/false
+// null  => standalone value
+// undefined  => 
+// symbol  => unique 
+
+
+// object
+
+console.log(typeof null);
