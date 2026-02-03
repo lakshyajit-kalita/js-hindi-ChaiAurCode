bee372e64e9f1e373e7aaa28f80dcdf12b78fe55
Lakshyajit kalita <144526326+lakshyajit-kalita@users.noreply.github.com>
2026-02-03 14:14:32 +0000

    function completed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,3 +34,55 @@ function loginUserMessage(userName = "Ram"){  // if we give the vlaue starting o
     return `${userName} user logged in`
 }
 console.log(loginUserMessage());   //if we did not give any value then it'll show undefine 
+
+//*****Rest operatior(...)******//
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 400, 5000));
+
+
+//*****Object in Function******//
+
+const user = {
+    bookname: "Lakshyajit ",
+    price: 500
+}
+
+function handleObject(anyobject){
+    console.log(`The book name is: ${anyobject.bookname} and its price is : ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    bookname: "Hi everyone",
+    price: 999
+})
+
+//*****Array in Function******//
+
+const nyNewArray = [2002, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(nyNewArray));
+
+
+// const myArray = [1, 2, 3, 4, 5]
+
+// function getTheThirdValue(getValue){
+//     return getValue[2]
+// }
+// console.log(getTheThirdValue(myArray));
+
+
+const myList = ["Hi", "shail", 1, 2 , 3]
+
+function getlastValue(getTheValue){
+    return getTheValue[4]
+}
+console.log(getlastValue(myList));
