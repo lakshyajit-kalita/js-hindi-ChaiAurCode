41e4505f1af0ecf8dab3fbbd8ebb86b9056140d8
Lakshyajit kalita <144526326+lakshyajit-kalita@users.noreply.github.com>
2026-02-04 19:22:10 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+let a = 100
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Inner: ",a);
+    function fName(){
+        
+    }
+    
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
