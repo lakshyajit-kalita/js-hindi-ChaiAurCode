af16d419a4dba41e8ae223d8e0e920129567afb4
Lakshyajit kalita <144526326+lakshyajit-kalita@users.noreply.github.com>
2026-02-05 03:25:05 +0000

    scopes completed

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,3 +15,28 @@ if(true){
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+function one(){
+    const userName = "Lakshya"
+    function two(){
+        const value = 2 
+        console.log(userName);
+    }
+    // console.log(value);
+    two()
+}
+
+one()
+
+ function addone(num){
+    return num+1
+ }
+console.log(addone(5))
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(10));
