5a88d420c092b9fe0c8f89b88bd9d4dcdf0069fc
Lakshyajit kalita <144526326+lakshyajit-kalita@users.noreply.github.com>
2026-02-11 13:54:12 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let i = 0
+// while (i <= 10) {
+//     console.log(`Values if index is: ${i}`);
+//     i += 1
+// }
+
+let superhero = ["flash", "superMan", "BatMan"]
+let arr = 0
+while (arr < superhero.length) {
+    console.log(`value is ${superhero[arr]}`);
+    arr += 1
+}
+
+///////////////////////////////Do While////////////////////////////////////////////////////////
+let score = 1
+do {
+    console.log(`The score is increasing by ${score}`);
+    score += 1
+} while(score<=10);
